2686e32d8f62bd0394518198c1af61d8e93758f1
Cole-Lowery <CLowery@student.neumont.edu>
2026-01-14 09:19:19 -0700

    Copied wrong unity project to this repo

---
+++ Assets/Tank/Scripts/Rocket.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Rocket : Ammo
+{
+    Rigidbody rb;
+    [SerializeField] GameObject effect;
+    [SerializeField] int speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddRelativeForce(Vector3.forward * speed, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //checking if object has health component
+        Health health = collision.gameObject.GetComponentInChildren<Health>();
+        
+        if (health != null)
+        {
+            //deal damage to hit object
+            health.OnDamage(1);
+        }
+        else
+        {
+            Debug.Log($"No health found on {collision.gameObject}");
+        }
+
+            Instantiate(effect, transform.position, Quaternion.identity);
+        //Destroy(effect, 2.5f);
+        Destroy(gameObject);
+    }
+}
