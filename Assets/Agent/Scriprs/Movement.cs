bf059845f68b69b57ee40d2fb36e19290fdf6cf9
Cole-Lowery <CLowery@student.neumont.edu>
2026-01-21 08:38:51 -0700

    seperating ai and tanks

--- Assets/Agent/Scriprs/Movement.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public abstract class Movement : MonoBehaviour
-{
-    public virtual Vector3 Velocity { get; set; }
-    public virtual Vector3 Acceleration { get; set; }
-
-}
--- Assets/Tank/Scripts/Ammo.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public abstract class Ammo : MonoBehaviour
-{
-    [SerializeField, Range(0.01f, 10.0f)] protected float damage = 1;
-}
